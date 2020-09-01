b31a6151b1aebed95f42a4533d4a098050bd75bc
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-01 15:19:32 +0530

    Add: Retrieve all notes by findAll

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -1,23 +1,33 @@
 const Note = require('../models/note.model.js');
 
 exports.create = (req, res) => {
-    if(!req.body.content) {
+    if (!req.body.content) {
         return res.status(400).send({
             message: "Note content can not be empty"
         });
     }
 
     const note = new Note({
-        title: req.body.title || "Untitled Note", 
+        title: req.body.title || "Untitled Note",
         content: req.body.content
     });
 
     note.save()
-    .then(data => {
-        res.send(data);
+        .then(data => {
+            res.send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Note."
+            });
+        });
+};
+
+exports.findAll = (req, res) => {
+    Note.find().then(notes => {
+        res.send(notes);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
-        });
-    });
-};
+            message: err.message || "Error occured while retreiving note"
+        })
+    })
+}
