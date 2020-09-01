3fd637ca15eded7f7290ea17c341c4e57f0c7d9f
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-01 15:27:54 +0530

    Add: Find note by Id using findById

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -31,3 +31,23 @@ exports.findAll = (req, res) => {
         })
     })
 }
+
+exports.findOne = (req, res) => {
+    Note.findById(req.params.noteId).then(note => {
+        if(!note){
+            return res.status(400).send({
+                message: "Note not found with id" + req.params.noteId;
+            })
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+        });
+    }
+       return res.status(500).send({
+
+       })
+    })
+}
