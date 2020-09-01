597c5291189aa6f8d532f04c31138507e0d7d36d
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-01 15:10:41 +0530

    Add: Create and save note

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,23 @@
+const Note = require('../models/note.model.js');
+
+exports.create = (req, res) => {
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    const note = new Note({
+        title: req.body.title || "Untitled Note", 
+        content: req.body.content
+    });
+
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
+};
