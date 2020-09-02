42a07070f0d1e2347a91fd5839ed77393e588348
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-02 13:22:01 +0530

    Add: Delete Note with noteId

--- app/routes/note.route.js
+++ app/routes/note.route.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
     app.post('/notes', notes.create);
     app.get('/notes', notes.findAll);
     app.get('/notes/:noteId',notes.findOne);
+    app.delete('/notes/:noteId',notes.delete);
 }
